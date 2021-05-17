b15767339df0a3a1cd5f952d2ceabeb4012a7ef4
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2021-05-17 21:22:05 +0200

    .

---
+++ main.py
@@ -0,0 +1,97 @@
+# %%
+import numpy as np
+
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+    if n > 0:
+        return np.random.random([n, 1])
+    return []
+    raise NotImplementedError
+
+def operations(h, w):
+    """
+    Takes two inputs, h and w, and makes two Numpy arrays A and B of size
+    h x w, and returns A, B, and s, the sum of A and B.
+
+    Arg:
+      h - an integer describing the height of A and B
+      w - an integer describing the width of A and B
+    Returns (in this order):
+      A - a randomly-generated h x w Numpy array.
+      B - a randomly-generated h x w Numpy array.
+      s - the sum of A and B.
+    """
+    A = np.random.random(size=(h,w))
+    B = np.random.random(size=(h,w))
+    s = A + B 
+    return  A, B, s
+    raise NotImplementedError
+
+
+def norm(A, B):
+    """
+    Takes two Numpy column arrays, A and B, and returns the L2 norm of their
+    sum.
+
+    Arg:
+      A - a Numpy array
+      B - a Numpy array
+    Returns:
+      s - the L2 norm of A+B.
+    """
+    s = A + B
+    return np.linalg.norm(s)
+    raise NotImplementedError
+
+
+def neural_network(inputs, weights):
+    """
+     Takes an input vector and runs it through a 1-layer neural network
+     with a given weight matrix and returns the output.
+
+     Arg:
+       inputs - 2 x 1 NumPy array
+       weights - 2 x 1 NumPy array
+     Returns (in this order):
+       out - a 1 x 1 NumPy array, representing the output of the neural network
+    """
+    out = np.tanh(weights.T.dot(inputs)) 
+    return out
+    raise NotImplementedError
+
+def scalar_function(x, y):
+    """
+    Returns the f(x,y) defined in the problem statement.
+    """
+    if(x > y):
+        return x/y
+    else:
+        return x*y
+    raise NotImplementedError
+
+def vector_function(x, y):
+    """
+    Make sure vector_function can deal with vector input x,y 
+    """
+    v_fn = np.vectorize(scalar_function)
+    return v_fn(x, y)
+    raise NotImplementedError
+
+
+
+# %%
+print(randomization(5))
+A, B, s = operations(3,5)
+print(A)
+print(B)
+print(s)
+print(norm(A,B))
+print(scalar_function(4,5))
+print(vector_function(4,5))
+
+# %%
